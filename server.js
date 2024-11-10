3112663d66b3627dbbab5098d90c47b9d3397892
bellatrick <busayosamuel2016@gmail.com>
2024-11-10 07:13:09 +0100

    cors error resolved

--- server.js
+++ server.js
@@ -9,11 +9,27 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:3000',"https://pgbookmarker.netlify.app/" ],
-    credentials: true
 
-  }));
+const allowedOrigins = [
+  "http://localhost:3000","https://pgbookmarker.netlify.app/"
+];
+
+// Configure CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
 
 // Configure Auth0 middleware
 const checkJwt = auth({
